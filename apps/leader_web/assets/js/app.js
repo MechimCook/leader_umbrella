1de6520aa7db5a3f426358778eda4c13728c3338
Mechim <mechim@getlua.com>
2020-04-15 14:01:02 -0400

    Form version 1

--- apps/leader_web/assets/js/app.js
+++ apps/leader_web/assets/js/app.js
@@ -1,19 +1,20 @@
-// We need to import the CSS so that webpack will load it.
-// The MiniCssExtractPlugin is used to separate it out into
-// its own CSS file.
+
 import css from "../css/app.css"
 
 import 'bootstrap';
 
-// webpack automatically bundles all modules in your
-// entry points. Those entry points can be configured
-// in "webpack.config.js".
-//
-// Import dependencies
-//
-import "phoenix_html"
 
-// Import local files
-//
-// Local files can be imported directly using relative paths, for example:
-// import socket from "./socket"
+window.onload = () => {
+  Array.from(document.querySelectorAll(".add-form-field"))
+  .forEach(el => {
+    el.onclick = ({target: {dataset}}) => {
+      let container = document.getElementById(dataset.container);
+      let index = container.dataset.index;
+      let newRow = dataset.prototype;
+      container.insertAdjacentHTML("beforeend",       newRow.replace(/__name__/g, index));
+      container.dataset.index = parseInt(container.dataset.index) + 1;
+    }
+  });
+}
+
+import "phoenix_html"
--- apps/leader_web/lib/leader_web/views/input_helpers.ex
+++ apps/leader_web/lib/leader_web/views/input_helpers.ex
@@ -11,52 +11,61 @@ defmodule LeaderWeb.InputHelpers do
 
     content_tag :div,
       id: container_id(id),
-      class: "form-group border rounded mt-2 mx-2" do
+      class: "form-group border rounded mt-2 mx-2",
+      data_index: 1 do
       values
       |> Enum.with_index()
-      |> Enum.map(fn {value, index} ->
-        new_id = id <> "_#{index}"
-        form_rows(form, new_id, rows)
+      |> Enum.map(fn {_value, index} ->
+        form_rows(form, id, index, rows)
       end)
     end
   end
 
-  defp form_rows(form, id, rows) do
+  defp form_rows(form, id, index, rows) do
+    new_id = id <> "_#{index}"
+
     content_tag :div,
       class: "form-group rounded border mt-2 mx-2",
-      id: id do
+      id: new_id do
       [
         Enum.reduce(rows, [], fn row, acc ->
-          [acc] ++ [form_row(form, row)]
-        end)
+          [acc] ++ [form_row(form, index, row)]
+        end),
+        content_tag :label,
+          onclick: "removeElement('#{new_id}')",
+          data: [id: new_id],
+          title: "Remove",
+          class: "remove-form-field text-primary font-weight-light ml-2" do
+          ["Remove"]
+        end
       ]
     end
   end
 
-  defp form_row(form, [class, columns]) do
+  defp form_row(form, index, [class, columns]) do
     content_tag :div,
       class: class do
       [
         Enum.reduce(columns, [], fn column, acc ->
-          [acc] ++ [form_column(form, column)]
+          [acc] ++ [form_column(form, index, column)]
         end)
       ]
     end
   end
 
-  defp form_column(form, [class, column]) do
+  defp form_column(form, index, [class, column]) do
     content_tag :div,
       class: class do
       [
         Enum.chunk_every(column, 2)
         |> Enum.reduce([], fn input, acc ->
-          [acc] ++ [form_input(form, input)]
+          [acc] ++ [form_input(form, index, input)]
         end)
       ]
     end
   end
 
-  defp form_input(form, [field, type]) do
+  defp form_input(form, index, [field, type]) do
     mapping = %{
       "url" => :url_input,
       "email" => :email_input,
@@ -79,70 +88,66 @@ defmodule LeaderWeb.InputHelpers do
       content_tag :div,
         class: "form-check form-check-inline",
         data: [index: Enum.count(values)] do
-        values
-        |> Enum.with_index()
-        |> Enum.map(fn {value, index} ->
-          new_id = id <> "_#{index}"
+        new_id = id <> "_#{index}"
 
-          input_opts = [
-            name: new_field_name(form, field),
-            value: value,
+        [
+          content_tag :input,
+            class: "mt-2",
+            name: new_field_name(form, index, field),
             id: new_id,
-            class: "mt-2"
-          ]
-
-          [
-            apply(Phoenix.HTML.Form, type, [form, field, input_opts]),
-            content_tag :label,
-              class: "ml-3" do
-              [to_string(field)]
-            end
-          ]
-        end)
+            type: "checkbox",
+            value: "true" do
+          end,
+          content_tag :label,
+            class: "ml-3" do
+            [to_string(field)]
+          end
+        ]
       end
     else
       content_tag :div,
         data: [index: Enum.count(values)] do
-        values
-        |> Enum.with_index()
-        |> Enum.map(fn {value, index} ->
-          new_id = id <> "_#{index}"
+        new_id = id <> "_#{index}"
 
-          input_opts = [
-            name: new_field_name(form, field),
-            value: value,
-            id: new_id
-          ]
+        input_opts = [
+          name: new_field_name(form, index, field),
+          id: new_id
+        ]
 
-          [
-            content_tag :label do
-              [to_string(field)]
-            end,
-            apply(Phoenix.HTML.Form, type, [form, field, input_opts])
-          ]
-        end)
+        [
+          content_tag :label do
+            [to_string(field)]
+          end,
+          apply(Phoenix.HTML.Form, type, [form, field, input_opts])
+        ]
       end
     end
   end
 
-  def order_add_button(form, field) do
-    # id = Phoenix.HTML.Form.input_id(form, field)
-    #
-    # content =
-    #   form_elements(form, field, "", "__name__")
-    #   |> safe_to_string
-    #
-    # data = [
-    #   prototype: content,
-    #   container: container_id(id)
-    # ]
-    #
-    # link("Add", to: "#", data: data, class: "add-form-field")
+  def order_add_button(form, field, rows) do
+    id = Phoenix.HTML.Form.input_id(form, field)
+
+    content =
+      form_rows(form, id, "__name__", rows)
+      |> safe_to_string
+
+    data = [
+      prototype: content,
+      container: container_id(id)
+    ]
+
+    content_tag :label,
+      onclick: "addElement()",
+      title: "Add",
+      data: data,
+      class: "add-form-field text-primary font-weight-light" do
+      ["Add"]
+    end
   end
 
   defp container_id(id), do: id <> "_container"
 
-  defp new_field_name(form, field) do
-    "order[" <> Phoenix.HTML.Form.input_name(form, field) <> "[]]"
+  defp new_field_name(form, index, field) do
+    "orders[#{index}][" <> Atom.to_string(field) <> "]"
   end
 end
