0f00b5b8bb1c7c3229a36f27c8177367f7179635
Mechim <mechim@getlua.com>
2020-04-30 15:10:41 -0400

    Generate excel of leads in DB

--- apps/leader/lib/leader/input.ex
+++ apps/leader/lib/leader/input.ex
@@ -5,11 +5,176 @@ defmodule Leader.Input do
 
   import Ecto.Query, warn: false
   alias Leader.Repo
+  alias Elixlsx.{Workbook, Sheet}
 
   alias Leader.Input.Lead
 
   @west_states ['WY', 'CO', 'UT', 'NV', 'ID', 'CA', 'OR', 'WA', 'AK', 'MT', 'FL']
   import Ecto.Query, only: [from: 2]
+
+  @lead_headers [
+    "Company",
+    "First_name",
+    "Last_name",
+    "Phone",
+    "Email",
+    "Address",
+    "City",
+    "State",
+    "Hot",
+    "Catalog",
+    "Comments"
+  ]
+
+  @products [
+    "Bottles",
+    "Jars",
+    "Roll_ons",
+    "Tubes"
+  ]
+
+  @materials [
+    "Aluminum",
+    "Glass",
+    "Plastic"
+  ]
+  @order_headers [
+    "Volume",
+    "Quantity",
+    "Products",
+    "Materials"
+  ]
+  def export() do
+    query =
+      from lead in Lead,
+        select: lead
+
+    query
+    |> Repo.all()
+    |> excel_generator()
+  end
+
+  def excel_generator(leads) do
+    # if not found is nil
+    # cannot search a nil map
+
+    rows =
+      leads
+      |> Enum.reduce([], fn lead, rows -> generate_row(lead) ++ rows end)
+
+    IO.inspect(%Workbook{sheets: [%Sheet{name: "leads", rows: [@lead_headers] ++ rows}]})
+
+    %Workbook{sheets: [%Sheet{name: "Leads", rows: [@lead_headers] ++ rows}]}
+    |> Elixlsx.write_to("hello.xlsx")
+  end
+
+  defp generate_row(lead) do
+    row = row(lead)
+
+    if Map.values(lead.orders) != [] && lead.orders != nil do
+      row = [row] ++ generate_orders(Map.values(lead.orders), [])
+    end
+  end
+
+  defp generate_orders([order | []], order_rows) do
+    final_order_rows =
+      case order do
+        %{
+          "Aluminum" => "false",
+          "Bottles" => "false",
+          "Glass" => "false",
+          "Jars" => "false",
+          "Plastic" => "false",
+          "Quantity" => "",
+          "Roll_ons" => "false",
+          "Tubes" => "false",
+          "Volume" => ""
+        } ->
+          order_rows
+
+        _ ->
+          order_rows ++ order_row(order)
+      end
+
+    if final_order_rows != [] do
+      [["Potental Orders"]] ++ [@order_headers] ++ [final_order_rows]
+    else
+      final_order_rows
+    end
+  end
+
+  defp generate_orders([order | tail], order_rows) do
+    case order do
+      %{
+        "Aluminum" => "false",
+        "Bottles" => "false",
+        "Glass" => "false",
+        "Jars" => "false",
+        "Plastic" => "false",
+        "Quantity" => "",
+        "Roll_ons" => "false",
+        "Tubes" => "false",
+        "Volume" => ""
+      } ->
+        generate_orders(tail, order_rows)
+
+      _ ->
+        generate_orders(tail, order_rows ++ order_row(order))
+    end
+  end
+
+  defp order_row(order) do
+    products =
+      @products
+      |> Enum.reduce(fn product, products ->
+        if Map.get(order, product) != "false" do
+          IO.inspect(Map.get(order, product))
+          product <> ", " <> products
+        else
+          products
+        end
+      end)
+
+    materials =
+      @materials
+      |> Enum.reduce(fn material, materials ->
+        if Map.get(order, material) != "false" do
+          material <> ", " <> materials
+        else
+          materials
+        end
+      end)
+
+    [Map.get(order, "Volume"), Map.get(order, "Quantity"), products, materials]
+  end
+
+  defp row(lead) do
+    [
+      lead.company,
+      lead.first_name,
+      lead.last_name,
+      lead.phone,
+      lead.email,
+      lead.address,
+      lead.city,
+      lead.state,
+      lead.hot,
+      lead.catalog,
+      lead.comments
+    ]
+    |> Enum.map(fn x ->
+      case x do
+        nil ->
+          ""
+
+        false ->
+          "No"
+
+        _ ->
+          x
+      end
+    end)
+  end
 
   def create_emails_for_day(date) do
     # date should be a day
